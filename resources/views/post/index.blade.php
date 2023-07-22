788853393e6a9538a61b9731e90727f50f1f3a42
Opnegreed <zaqqq12@gmail.com>
2023-07-22 23:11:13 +0300

    third commit

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app')
 @section('content')
-<div>post</div>
+    <table class="table">
+        @foreach($posts as $post)
+        <thead>
+        <tr>
+            <th scope="col">{{ $post -> id }}</th>
+            <th scope="col">{{ $post -> title }}</th>
+            <th scope="col">{{ $post -> content }}</th>
+        </tr>
+        </thead>
+        @endforeach
+    </table>
 @endsection
